4905d7c3760f2723daaf81baac469bd4da3113ae
Enixor <enixor@ctemplar.com>
2022-01-14 22:01:54 +0100

    Decrease indent

--- src/main/java/io/github/zrdzn/minecraft/flameregions/FlameRegionsPlugin.java
+++ src/main/java/io/github/zrdzn/minecraft/flameregions/FlameRegionsPlugin.java
@@ -75,10 +75,10 @@ public class FlameRegionsPlugin extends JavaPlugin {
         }
 
         String query = "CREATE TABLE IF NOT EXISTS explored_regions (" +
-                "id INT NOT NULL PRIMARY KEY AUTO_INCREMENT," +
-                "region_name VARCHAR(128) NOT NULL UNIQUE KEY," +
-                "explorer_uuid VARCHAR(36)," +
-                "explorer_name VARCHAR(18));";
+            "id INT NOT NULL PRIMARY KEY AUTO_INCREMENT," +
+            "region_name VARCHAR(128) NOT NULL UNIQUE KEY," +
+            "explorer_uuid VARCHAR(36)," +
+            "explorer_name VARCHAR(18));";
         try (Connection connection = dataSource.getConnection(); PreparedStatement statement = connection.prepareStatement(query)) {
             statement.executeUpdate();
         } catch (SQLException exception) {
